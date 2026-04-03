a2c5364888e35462a1a7f226dc72e45ad777251d
Aman Raj <2025sl93091@wilp.bits-pilani.ac.in>
2026-04-03 16:59:14 +0530

    Add divide function

--- calculator.py
+++ calculator.py
@@ -1,1 +1,2 @@
-def add(a, b):\n    return a + b\n\ndef multiply(a, b):\n    return a * b\n\ndef subtract(a, b):\n    return a - b
+def subtract(a, b):\n    return a - b
+def add(a, b):\n    return a + b\n\ndef multiply(a, b):\n    return a * b\n\ndef divide(a, b):\n    if b == 0:\n        return None\n    return a / b
